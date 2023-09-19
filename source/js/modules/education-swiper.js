9bde20753d0315198e279aaf3c5611ab71e0848f
Инга Егонская <ivegon@mail.ru>
2023-09-19 23:11:20 +0700

    Размечает и стилизует футер, исправляет ошибку блока Вопросы

--- source/js/modules/education-swiper.js
+++ source/js/modules/education-swiper.js
@@ -1,31 +1,31 @@
 const swiperInstructors = function () {
-    // eslint-disable-next-line
-    new Swiper('.education__instructors-swiper', {
+  // eslint-disable-next-line
+  new Swiper('.education__instructors-swiper', {
 
-        navigation: {
-            nextEl: '.education__instructors-button--next',
-            prevEl: '.education__instructors-button--back',
-        },
+    navigation: {
+      nextEl: '.education__instructors-button--next',
+      prevEl: '.education__instructors-button--back',
+    },
 
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 30,
-            },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
 
-            // when window width is >= 768px
-            768: {
-                slidesPerView: 3,
-                spaceBetween: 18,
-            },
-            // when window width is >= 1200px
-            1200: {
-                slidesPerView: 4,
-                spaceBetween: 30,
-                allowTouchMove: false,
-            },
-        },
-    });
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 18,
+      },
+      // when window width is >= 1200px
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        allowTouchMove: false,
+      },
+    },
+  });
 };
 
 export {swiperInstructors};
--- source/js/modules/feedback-swiper.js
+++ source/js/modules/feedback-swiper.js
@@ -1,34 +1,34 @@
 const swiperFeedback = function () {
-    // eslint-disable-next-line
-    new Swiper('.feedback__swiper', {
+  // eslint-disable-next-line
+  new Swiper('.feedback__swiper', {
 
-        navigation: {
-            nextEl: '.feedback__button--forward',
-            prevEl: '.feedback__button--back',
-        },
+    navigation: {
+      nextEl: '.feedback__button--forward',
+      prevEl: '.feedback__button--back',
+    },
 
-        autoHeight: true,
+    autoHeight: true,
 
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 30,
-            },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
 
-            // when window width is >= 768px
-            768: {
-                slidesPerView: 'auto',
-                spaceBetween: 30,
-                allowTouchMove: true,
-            },
-            // when window width is >= 1200px
-            1200: {
-                slidesPerView: 'auto',
-                spaceBetween: 30,
-                allowTouchMove: false,
-            },
-        },
-    });
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+        allowTouchMove: true,
+      },
+      // when window width is >= 1200px
+      1200: {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+        allowTouchMove: false,
+      },
+    },
+  });
 };
 
 export {swiperFeedback};
